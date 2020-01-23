538a7f60a5461f327c2c3ff2b45ea31c68676172
MatFi <mathias@liebeleu.de>
2020-01-23 12:38:09 +0100

    adjusted precision for iv measure

--- src/Common/solve.jl
+++ src/Common/solve.jl
@@ -1,4 +1,4 @@
-function solve(c::Cell)
+function solve(c::Cell;tend=20)
     if (c.initialized==:false)
         initial_conditions!(c)
     #    if c.initialized != :Sucess
@@ -6,21 +6,22 @@ function solve(c::Cell)
     #    end
     end
 
-    prob = get_problem(c)
+    prob = get_problem(c,tend=tend)
 
     tol_callback = AutoAbstol(false;init_curmax=c.u0 .+ 000.00001)
     (sav_callback, sav_sol) = SavingCallback(c)
     callbacks = CallbackSet(sav_callback, tol_callback )
     @info "Solve"
+    #TODO: abstol needs to bedone more nice
     sol = solve(prob,c.alg_ctl.alg;
         progress_steps = 50,
         progress = true,
-        callback = callbacks,
+        callback = sav_callback,#callbacks,
         dt =  1e-15,
         dtmin = 1e-20,# ustrip(1e-20*c.parameters.τᵢ), #1e-20,
         force_dtmin = true,
         reltol = c.alg_ctl.reltol,
-        abstol = c.u0 .* 0, #1e-12,#c.u0 .* 0,
+        abstol = 1e-12,#c.u0 .* 0, #1e-12,#c.u0 .* 0,
         maxiters= 5000
     )
     c.sol = sav_sol
@@ -37,10 +38,8 @@ function get_problem(c::Cell;tend = 20.0)
         colorvec = matrix_colors(c.Jac),
     )
 
-    tspan = (0.0,5000*π/ustrip(c.parameters.τᵢ))
-    if c.mode==:oc
-        tspan = (0.0,ustrip(tend/c.parameters.τᵢ))
-    end
+    tspan = (0.0,ustrip(tend/c.parameters.τᵢ))
+
 
 
     tol_callback = AutoAbstol(false;init_curmax=u0 .+ 0.1)
