362735ae8ffa31156eb29a9fa2320a7ca6b75eeb
MatFi <git@endof.de>
2020-02-05 20:13:15 +0100

    allow array sin params.. for AnnAOptim

--- src/parameters.jl
+++ src/parameters.jl
@@ -1,75 +1,74 @@
 abstract type AbstractParameters end
-
+const vPar = Union{Number,Tuple,AbstractArray,Function,Array{Function,1}}
 Base.@kwdef mutable struct Parameters <: AbstractParameters
-
     """Model Parameters"""
 
     N::Integer  =   400     # Subintervals in perovskite layer, resulting in N+1 Grid points
-    rtol::Float64 = 1e-6    # Relative tolerance for the solver (abtol is automatic)
+
 
     #Physical parameters
-    ε₀::Number = 8.854187817e-12u"F/m"  # Permitivity of free space
-    q::Number  = 1.6021766209e-19u"C"   # Elemntary charge of a proton
-    kB::Number = 8.61733035e-5u"eV/K"  # Bolzmann konstant
-    T::Number  = 300u"K"                # Temperature
+    ε₀::vPar = 8.854187817e-12u"F/m"  # Permitivity of free space
+    q::vPar  = 1.6021766209e-19u"C"   # Elemntary charge of a proton
+    kB::vPar = 8.61733035e-5u"eV/K"  # Bolzmann konstant
+    T::vPar  = 300u"K"                # Temperature
 
     # Perovskite parameters
-    b::Number  = 400e-9u"m"           # Perovskite Layer thickness
-    ε::Number = 24.1
-    #εₚ::Number = ε*ε₀            # Perovskite permitivity
-    Ec::Number = -3.7u"eV"        # Perovskite Conduction band energy
-    Ev::Number = -5.3u"eV"        # Perovskite Valence band energy
-    #Eg::Number = Ec-Ev              # Perovskite Bandgap
-    Dₙ::Number = 1.7e-4u"m^2/s"     # Perovskite electron diffusion coefficient
-    Dₚ::Number = 1.7e-4u"m^2/s"     # Perovskite hole diffusion coefficient
-    gc::Number = 8.1e24u"m^-3"      # Perovskite conduction band DOS
-    gv::Number = 5.8e24u"m^-3"      # Perovskite valence band DOS
+    b::vPar  = 400e-9u"m"           # Perovskite Layer thickness
+    ε::vPar = 24.1
+    #εₚ::vPar = ε*ε₀            # Perovskite permitivity
+    Ec::vPar = -3.7u"eV"        # Perovskite Conduction band energy
+    Ev::vPar = -5.3u"eV"        # Perovskite Valence band energy
+    #Eg::vPar = Ec-Ev              # Perovskite Bandgap
+    Dₙ::vPar = 1.7e-4u"m^2/s"     # Perovskite electron diffusion coefficient
+    Dₚ::vPar = 1.7e-4u"m^2/s"     # Perovskite hole diffusion coefficient
+    gc::vPar = 8.1e24u"m^-3"      # Perovskite conduction band DOS
+    gv::vPar = 5.8e24u"m^-3"      # Perovskite valence band DOS
     # Ion Parameters
-    N₀::Number = 1.6e25u"m^-3"      # Typical density of ion vacancys
-    Dᵢ₀::Number= 6.5e-8u"m^2/s"     # Diffusion constant
-    Eᵢₐ::Number= 0.58*q*u"V"        # Ativation energy of vacancy diffusion
-#    Dᵢ::Number = Dᵢ₀*exp(-Eᵢₐ/(kB*T)) #ext #Diffusion coefficient of ions
+    N₀::vPar = 1.6e25u"m^-3"      # Typical density of ion vacancys
+    Dᵢ₀::vPar= 6.5e-8u"m^2/s"     # Diffusion constant
+    Eᵢₐ::vPar= 0.58*q*u"V"        # Ativation energy of vacancy diffusion
+#    Dᵢ::vPar = Dᵢ₀*exp(-Eᵢₐ/(kB*T)) #ext #Diffusion coefficient of ions
 
     # Light Parameters
-    α::Number  = 1.3e7u"1/m"        # Perovskite absorption koefficient
-    Fₚₕ::Number= 1.4e21u"m^-2*s^-1" # Photonflux (bandgap dependent)
+    α::vPar  = 1.3e7u"1/m"        # Perovskite absorption koefficient
+    Fₚₕ::vPar= 1.4e21u"m^-2*s^-1" # Photonflux (bandgap dependent)
     dir::Integer =   1              # Shine light trough  1 -> ETL, -1 -> HTL
-    light::Function = pulse(tₑ=1.0)
+    light::vPar = pulse(tₑ=1.0)
 
     # Recombination Parameters
-    #nᵢ::Number = sqrt(gc*gv)*exp(-Eg/(2*kB*T)) #ext  #Intrinsic carrier density
-    τₙ::Number = 3e-9u"s"           #electron pseudo lifetime
-    τₚ::Number = 3e-7u"s"           # hole pseudo lifetime
-    k₂::Number = 0u"m^3/s"          # second order rate constant
+    #nᵢ::vPar = sqrt(gc*gv)*exp(-Eg/(2*kB*T)) #ext  #Intrinsic carrier density
+    τₙ::vPar = 3e-9u"s"           #electron pseudo lifetime
+    τₚ::vPar = 3e-7u"s"           # hole pseudo lifetime
+    k₂::vPar = 0u"m^3/s"          # second order rate constant
     # Interface Recombination
-    k₂ₑ::Number = 0u"m^4/s"         # ETL/perovskite bimolecular recombination rate
-    k₂ₕ::Number = 0u"m^4/s"         # perovskite/HTL bimolecular recombination rate
-    vₙₑ::Number = 1e5u"m/s"         # electron recombination velocity for SHR/ETL
-    vₚₑ::Number = 10u"m/s"          # hole recombination velocity for SHR/ETL
-    vₙₕ::Number = 0.1u"m/s"         # electron recombination velocity for SHR/HTL
-    vₚₕ::Number = 1e5u"m/s"         # hole recombination velocity for SHR/HTL
+    k₂ₑ::vPar = 0u"m^4/s"         # ETL/perovskite bimolecular recombination rate
+    k₂ₕ::vPar = 0u"m^4/s"         # perovskite/HTL bimolecular recombination rate
+    vₙₑ::vPar = 1e5u"m/s"         # electron recombination velocity for SHR/ETL
+    vₚₑ::vPar = 10u"m/s"          # hole recombination velocity for SHR/ETL
+    vₙₕ::vPar = 0.1u"m/s"         # electron recombination velocity for SHR/HTL
+    vₚₕ::vPar = 1e5u"m/s"         # hole recombination velocity for SHR/HTL
 
     # ELT Parameters
-    dₑ::Number = 1e24u"m^-3"        # ETL effective doping density
-    gcₑ::Number= 5e25u"m^-3"        # ETL conduction band DOS
-    Ecₑ::Number= -4.0*u"eV"          # ETL conduction band energy
-    bₑ::Number = 100e-9u"m"         # ETL width
+    dₑ::vPar = 1e24u"m^-3"        # ETL effective doping density
+    gcₑ::vPar= 5e25u"m^-3"        # ETL conduction band DOS
+    Ecₑ::vPar= -4.0*u"eV"          # ETL conduction band energy
+    bₑ::vPar = 100e-9u"m"         # ETL width
     εₑᵣ::Real = 3
-    #εₑ::Number = εₑᵣ*ε₀               # ETL permitivity
-    Dₑ::Number = 1e-5u"m^2/s"       # ETL electron diffusion coeficcient
-    #Efₑ::Number = Ecₑ-kB*T*log(gcₑ /dₑ) #ext #HTL Fermi level on edge
+    #εₑ::vPar = εₑᵣ*ε₀               # ETL permitivity
+    Dₑ::vPar = 1e-5u"m^2/s"       # ETL electron diffusion coeficcient
+    #Efₑ::vPar = Ecₑ-kB*T*log(gcₑ /dₑ) #ext #HTL Fermi level on edge
     # HTL Parameters
-    dₕ::Number = 1e24u"m^-3"        # HTL effective doping density
-    gvₕ::Number= 5e25u"m^-3"        # HTL valence band DOS
-    Evₕ::Number= -5*u"eV"          # HTL valence band energy
-    bₕ::Number = 100e-9u"m"         # HTL width
+    dₕ::vPar = 1e24u"m^-3"        # HTL effective doping density
+    gvₕ::vPar= 5e25u"m^-3"        # HTL valence band DOS
+    Evₕ::vPar= -5*u"eV"          # HTL valence band energy
+    bₕ::vPar = 100e-9u"m"         # HTL width
     εₕᵣ::Real = 3
-#    εₕ::Number = εₕᵣ*ε₀               # HTL permitivity
-    Dₕ::Number = 1e-5u"m^2/s"       # HTL electron diffusion coeficcient
-    #Efₕ::Number = Evₕ+kB*T*log(gvₕ /dₕ) #ext #HTL Fermi level on edge
+#    εₕ::vPar = εₕᵣ*ε₀               # HTL permitivity
+    Dₕ::vPar = 1e-5u"m^2/s"       # HTL electron diffusion coeficcient
+    #Efₕ::vPar = Evₕ+kB*T*log(gvₕ /dₕ) #ext #HTL Fermi level on edge
 
-    V::Function = t-> 0#uconvert(Unitful.NoUnits,(Efₑ-Efₕ)/1u"V")      # Voltage protocoll
-    Rₛₕ::Number = 1e3u"V/A*m^2"     # shunt resistance
+    V::vPar = t-> 0#uconvert(Unitful.NoUnits,(Efₑ-Efₕ)/1u"V")      # Voltage protocoll
+    Rₛₕ::vPar = 1e3u"V/A*m^2"     # shunt resistance
 
 end
 
@@ -94,9 +93,9 @@ getproperty(p::AbstractParameters,::Val{S}) where {S} = getfield(p,S)
 getproperty(p::AbstractParameters,::Val{:εₚ}) = p.ε*p.ε₀
 getproperty(p::AbstractParameters,::Val{:εₕ}) = p.εₕᵣ*p.ε₀
 getproperty(p::AbstractParameters,::Val{:εₑ}) = p.εₑᵣ*p.ε₀
-getproperty(p::AbstractParameters,::Val{:Eg}) = p.Ec-p.Ev
+getproperty(p::AbstractParameters,::Val{:Eg}) = p.Ec-p.Ev |> u"eV"
 getproperty(p::AbstractParameters,::Val{:Dᵢ}) = p.Dᵢ₀*exp(-p.Eᵢₐ/(p.kB*p.T)) |> u"m^2/s"
-getproperty(p::AbstractParameters,::Val{:nᵢ}) = sqrt(p.gc*p.gv)*exp(-p.Eg/(2*p.kB*p.T))
+getproperty(p::AbstractParameters,::Val{:nᵢ}) = sqrt(p.gc*p.gv)*exp(-p.Eg/(2*p.kB*p.T))  |> u"m^-3"
 getproperty(p::AbstractParameters,::Val{:Efₑ}) = p.Ecₑ-p.kB*p.T*log(p.gcₑ /p.dₑ) |> u"eV"
 getproperty(p::AbstractParameters,::Val{:Efₕ}) = p.Evₕ+p.kB*p.T*log(p.gvₕ /p.dₕ) |> u"eV"
 getproperty(p::AbstractParameters,::Val{:Vbi}) = (p.Efₑ-p.Efₕ) |> u"eV"
