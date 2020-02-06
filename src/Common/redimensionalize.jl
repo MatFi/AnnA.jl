9e11cfcff574b4889bed86a364cd654f68f57e48
MatFi <git@endof.de>
2020-02-06 21:37:19 +0100

    more docs

--- src/Common/redimensionalize.jl
+++ src/Common/redimensionalize.jl
@@ -1,4 +1,8 @@
+"""
+    rdim_sol(sol,p::AbstractParameters)
 
+a single time slice `sol` of the ODESolution is redimensionalized with `p`
+"""
 function rdim_sol(sol,p::AbstractParameters)
 #    [[P], [ϕₑ,ϕ,ϕₕ], [nₑ,n], [p,pₕ]]
     P   = sol[1].*p.N₀
@@ -11,15 +15,28 @@ end
 
 rdim_sol(c::Cell,sol::Array{T,1} where T<:Real) = rdim_sol(decompose(sol,c.g), c.parameters) # legacy wrapper
 #rdim_sol(sol::Vector{Float64},p::AbstractParameters) = rdim_sol(sol,p)
+
+"""
+    rdim_sol(sol::DiffEqBase.ODESolution)
+
+Redimensionalizes the complete ODESolution at each time step.
+"""
 function rdim_sol(sol::DiffEqBase.ODESolution)
     p = sol.prob.f.f
     u = decompose.(sol.u,p.g)
     u_ret = rdim_sol.(u,(p.parameters,))
     return u_ret
 end
+
+"""
+    rdim_sol(sol::DiffEqBase.ODESolution,t)
+
+returns a redimensionalized solution vector at time `t`, the time is treated as
+seconds if no unit is given.
+"""
 function rdim_sol(sol::DiffEqBase.ODESolution,t)
     p = sol.prob.f.f
-    u = decompose(sol(t),p.g)
+    u = decompose(sol(ustrip(upreferred(t/p.parameters.τᵢ))),p.g)
     u_ret = rdim_sol(u,p.parameters)
     return u_ret
 end
