b16015c8694cd868cb2a438dbb1083b772e071dc
MatFi <git@endof.de>
2020-12-07 23:01:32 +0100

    fix tests

--- test/problems_test.jl
+++ test/problems_test.jl
@@ -2,15 +2,15 @@
     parameters= AnnABase.Parameters(light = t->0,Rₛₕ =Inf*1u"V/A*m^2" )
     prob = AnnABase.IVProblem(parameters,[-0.2,1.4]u"V",0.005u"V/s")
     @test prob isa AnnABase.IVProblem
-    @test  AnnABase.solve(prob) isa AnnABase.IVSolution
+    @test  AnnABase.solve(prob) isa AnnABase.ProblemSolution
 
     #forward direction
     prob = AnnABase.IVProblem(parameters,[-0.2,1.4]u"V",0.005u"V/s",double_sweep=false)
-    @test  AnnABase.solve(prob) isa AnnABase.IVSolution
+    @test  AnnABase.solve(prob) isa AnnABase.ProblemSolution
 
     #reverse direction
     prob = AnnABase.IVProblem(parameters,[1.2,0.4]u"V",0.005u"V/s",double_sweep=false)
-    @test  AnnABase.solve(prob) isa AnnABase.IVSolution
+    @test  AnnABase.solve(prob) isa AnnABase.ProblemSolution
 
     #test the ocvd
     prob_ocvd = AnnABase.OCVDProblem(parameters,10u"s",1e3u"s")
