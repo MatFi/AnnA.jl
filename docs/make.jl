f7ff94c6a61b27ec35d4d122986c87e9a9f0c514
MatFi <git@endof.de>
2021-04-09 22:09:24 +0200

    fix test

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,16 @@ using Documenter
 using AnnABase
 
 makedocs(
-    sitename = "AnnABase Documentation",
-    modules = [AnnABase],
+    sitename = "AnnA.jl",
+    modules = [AnnA],
     doctest = true,
     strict = true,
     checkdocs = :exports,
-    repo = "https://gitlab.com/MatFi/AnnABase.jl/blob/{commit}{path}#{line}",
+#    repo = "https://gitlab.com/MatFi/AnnABase.jl/blob/{commit}{path}#{line}",
     pages = Any["Home"=>"index.md", "API"=>"api.md"],
+)
+
+
+deploydocs(
+   repo = "https://github.com/MatFi/AnnA.jl"
 )
