52600e93e69068a33daa5347789135e62ef6c073
MatFi <git@endof.de>
2021-04-10 12:42:27 +0200

    fix doctest

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,12 @@ push!(LOAD_PATH, "../src/")
 using Documenter
 using AnnA
 
-DocMeta.setdocmeta!(AnnA, :DocTestSetup, :(using AnnA); recursive=true)
+#DocMeta.setdocmeta!(AnnA, :DocTestSetup, :(using AnnA); recursive=true)
 
 makedocs(
     sitename = "AnnA.jl",
     modules = [AnnA],
-    doctest = fase,
+    doctest = true,
     strict = true,
     checkdocs = :exports,
 #    repo = "https://gitlab.com/MatFi/AnnABase.jl/blob/{commit}{path}#{line}",
